979ce70bfc806fe52463fbdd5013efe8e3a89034
ABDURRAHMAN <romaissaeabde@gmail.com>
2024-11-28 20:51:59 +0100

    [ ADD ] FTP Config on docker compose.

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-
 service vsftpd start
-
 
 adduser $ftp_user --disabled-password
 
 echo "$ftp_user:$ftp_pwd" | /usr/sbin/chpasswd
-
 echo "$ftp_user" | tee -a /etc/vsftpd.userlist 
 
-
-mkdir /home/$ftp_user/ftp
-
+mkdir -p  /home/$ftp_user/ftp
 
 chown nobody:nogroup /home/$ftp_user/ftp
 chmod a-w /home/$ftp_user/ftp
@@ -33,6 +28,5 @@ pasv_max_port=40005
 userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 
 service vsftpd stop
-
 
 /usr/sbin/vsftpd
